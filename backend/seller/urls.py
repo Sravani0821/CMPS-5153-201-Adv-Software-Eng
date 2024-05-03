5a65db2daa1d90ea39360969fa89856963200ddf
Sravani0821 <123595425+Sravani0821@users.noreply.github.com>
2024-05-03 09:19:37 -0500

    Add files via upload

--- backend/seller/urls.py
+++ backend/seller/urls.py
@@ -5,4 +5,15 @@ from . import views
 
 urlpatterns = [
     path("register/", views.SellerRegisterView.as_view(), name="seller_register"),
+    path("dashboard/", views.SellerDashboardView.as_view(), name="seller_dashboard"),
+    path(
+        "dashboard/store/",
+        views.SellerDashboardStoreView.as_view(),
+        name="seller_dashboard_store",
+    ),
+    path(
+        "dashboard/lottery/",
+        views.SellerDashboardLotteryView.as_view(),
+        name="seller_dashboard_lottery",
+    ),
 ]
--- backend/seller/views.py
+++ backend/seller/views.py
@@ -4,6 +4,19 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 
+from item.models import (
+    StoreItemModel,
+    StoreItemBidModel,
+    LotteryBasedAuctionModel,
+    LotteryBasedAuctionBidModel,
+)
+from item.serializers import (
+    StoreItemSerializer,
+    StoreItemBidSerializer,
+    LotteryBasedAuctionSerializer,
+    LotteryBasedAuctionBidSerializer,
+)
+
 from . import serializers
 from . import models
 
@@ -44,6 +57,62 @@ class SellerRegisterView(APIView):
             )
             serializer = serializers.SellerSerializer(seller)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response(
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR, data={"error": str(e)}
+            )
+
+
+class SellerDashboardView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        try:
+            seller = models.SellerModel.objects.get(user=request.user)
+            recent_items = StoreItemModel.objects.filter(seller=seller)[0:5]
+            recent_auctions = LotteryBasedAuctionModel.objects.filter(seller=seller)[
+                0:5
+            ]
+            recent_store_item_serializer = StoreItemSerializer(recent_items, many=True)
+            recent_lottery_auction_serializer = LotteryBasedAuctionSerializer(
+                recent_auctions, many=True
+            )
+            return Response(
+                {
+                    "recent_store_items": recent_store_item_serializer.data,
+                    "recent_lottery_items": recent_lottery_auction_serializer.data,
+                }
+            )
+        except Exception as e:
+            return Response(
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR, data={"error": str(e)}
+            )
+
+
+class SellerDashboardStoreView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        try:
+            seller = models.SellerModel.objects.get(user=request.user)
+            items = StoreItemModel.objects.filter(seller=seller)
+            serializer = StoreItemSerializer(items, many=True)
+            return Response(serializer.data)
+        except Exception as e:
+            return Response(
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR, data={"error": str(e)}
+            )
+
+
+class SellerDashboardLotteryView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        try:
+            seller = models.SellerModel.objects.get(user=request.user)
+            auctions = LotteryBasedAuctionModel.objects.filter(seller=seller)
+            serializer = LotteryBasedAuctionSerializer(auctions, many=True)
+            return Response(serializer.data)
         except Exception as e:
             return Response(
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR, data={"error": str(e)}
