ba478fed8b20bcb91f741dbe258c9cbe5aa4593f
Sravani0821 <123595425+Sravani0821@users.noreply.github.com>
2024-05-03 09:14:01 -0500

    Add files via upload

--- backend/authentication/apps.py
+++ backend/authentication/apps.py
@@ -1,9 +1,6 @@
-# Importing the AppConfig class from the django.apps module
 from django.apps import AppConfig
 
-# Defining a new configuration class named AuthenticationConfig.
+
 class AuthenticationConfig(AppConfig):
-     # Setting the default_auto_field attribute to specify the type of auto-generated primary key field for models.
     default_auto_field = "django.db.models.BigAutoField"
-    # Setting the name attribute to specify the full Python path to the application.
     name = "authentication"
--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,12 +1,8 @@
-# Importing the path function from the django.urls
 from django.urls import path
 from . import views
-# Defining URL patterns for the application.
+
 urlpatterns = [
-    # Mapping the URL path "login/" to the LoginView class-based view with the name "login".
     path("login/", views.LoginView.as_view(), name="login"),
-    # Mapping the URL path "logout/" to the LogoutView class-based view with the name "logout".
     path("logout/", views.LogoutView.as_view(), name="logout"),
- # Mapping the URL path "user/" to the UserView class-based view with the name "user".
     path("user/", views.UserView.as_view(), name="user"),
 ]
