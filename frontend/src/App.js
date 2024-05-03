6abd2e88f6904665b9250e82ddcc57f32e59c4ab
Sravani0821 <123595425+Sravani0821@users.noreply.github.com>
2024-05-03 09:29:50 -0500

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from "axios";
 import Login from "./components/auth/login";
 import Register from "./components/auth/register";
 import RegisterAsSeller from "./components/auth/register-as-seller";
@@ -10,7 +11,16 @@ import Home from "./components/home";
 import Page404 from "./components/page-404";
 import StoreHome from "./components/store/store-home";
 import StoreItemDetail from "./components/store/store-item-detail";
-import axios from "axios";
+import StoreItemCreate from "./components/store/store-item-create";
+import LottreyHome from "./components/lottery/lottery-home";
+import LotteryItemCreate from "./components/lottery/lottery-item-create";
+import LotteryItemDetail from "./components/lottery/lottery-item-detail";
+import Contact from "./components/contact";
+import SellerDashboard from "./components/dashboard/seller/dashboard";
+import SellerDashboardStore from "./components/dashboard/seller/store";
+import SellerDashboardLottery from "./components/dashboard/seller/lottery";
+import BidderDashboard from "./components/dashboard/bidder/dashboard";
+import Notifications from "./components/notifications";
 
 axios.defaults.baseURL = "http://127.0.0.1:8000";
 axios.defaults.headers.post["Content-Type"] = "application/json";
@@ -46,7 +56,23 @@ function App() {
         <Route path="/register/bidder" element={<RegisterAsBidder />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/store" element={<StoreHome />} />
+        <Route path="/store/create" element={<StoreItemCreate />} />
         <Route path="/store/item/:itemId" element={<StoreItemDetail />} />
+        <Route path="/lottery" element={<LottreyHome />} />
+        <Route path="/lottery/create" element={<LotteryItemCreate />} />
+        <Route path="/lottery/item/:itemId" element={<LotteryItemDetail />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/dashboard/seller" element={<SellerDashboard />} />
+        <Route
+          path="/dashboard/seller/store"
+          element={<SellerDashboardStore />}
+        />
+        <Route
+          path="/dashboard/seller/lottery"
+          element={<SellerDashboardLottery />}
+        />
+        <Route path="/dashboard/bidder" element={<BidderDashboard />} />
+        <Route path="/notifications" element={<Notifications />} />
       </Routes>
     </BrowserRouter>
   );
